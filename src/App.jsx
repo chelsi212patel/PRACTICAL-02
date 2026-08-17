9fcfdf9985ec3a321e6ef98bef33e4042f03b3cb
chelsi212patel <patelchelsi02@gmail.com>
2026-08-17 09:50:50 +0530

    PRACTICAL-02.git

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Student from "./Student";
+
+function App() {
+  return (
+    <div>
+      <h1>Student Details</h1>
+
+      <Student
+        name="Chelsi Patel"
+        rollNo="087"
+        course="BCA"
+        marks="85%"
+      />
+
+      <Student
+        name="Dvija Patel"
+        rollNo="102"
+        course="BCA"
+        marks="92%"
+      />
+
+      <Student
+        name="Krishna Patel"
+        rollNo="103"
+        course="BCA"
+        marks="78%"
+      />
+    </div>
+  );
+}
+
+export default App;
